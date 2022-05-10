7d8c1b076bdb4dcf5ee4fe882f8da3d341916c67
[bonface221] <[mainaboniface221@gmail.com]>
2022-05-10 14:59:58 +0300

    ft:added the app-models file

---
+++ app/models.py
@@ -0,0 +1,51 @@
+from . import db
+from flask_login import UserMixin
+from . import login_manager
+from werkzeug.security import generate_password_hash,check_password_hash
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(UserMixin,db.Model):
+    __tablename__='users'
+    id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(255),index = True)
+    email= db.Column(db.String(255),unique = True, index = True)
+    pass_secure = db.Column(db.String(455))
+    pitches=db.relationship('Pitches',backref='user',lazy='dynamic')
+    profile_pic_path= db.Column(db.String())
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self,password):
+        self.pass_secure = generate_password_hash(password, method='pbkdf2:sha256', salt_length=8)
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+
+    
+    def __repr__(self):
+        return f'User {self.username}'
+
+class Pitches(db.Model):
+    __tablename__='pitches'
+    id = db.Column(db.Integer, primary_key=True)
+    title=db.Column(db.String(255))
+    description=db.Column(db.String(255))
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+
+
+    
+
+
+class Coments(db.Model):
+    __tablename__='coments'
+    id=db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(255))
+    comment= db.Column(db.String(255))
