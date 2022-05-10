f6b050031c718c556368afd11807fc162eefb615
[bonface221] <[mainaboniface221@gmail.com]>
2022-05-10 15:59:20 +0300

    changes

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from config import config_options
 from flask_login import LoginManager
-from flask_uploads import UploadSet,configure_uploads,IMAGES
+#from flask_uploads import UploadSet,configure_uploads,IMAGES
 from flask_mail import Mail
 
 
@@ -14,7 +14,7 @@ login_manager.login_view = 'auth.login'
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
-photos = UploadSet('photos',IMAGES)
+# photos = UploadSet('photos',IMAGES)
 mail = Mail()
 
 
